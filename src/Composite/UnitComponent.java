18f41568be6e68e31e841a1d8b755512eb027f16
Simon <100418694+SimonXX@users.noreply.github.com>
2023-06-19 22:58:06 +0200

    Definizione classi Base
    Applicazione Pattern Composite

---
+++ src/Composite/UnitComponent.java
@@ -0,0 +1,23 @@
+package Composite;
+
+
+import java.util.List;
+
+public interface UnitComponent {
+    String getName();
+    void addEmployee(Employee employee);
+    void removeEmployee(Employee employee);
+    List<Employee> getEmployees();
+    void addSubUnit(Unit subUnit);
+    void removeSubUnit(Unit subUnit);
+    List<Unit> getSubUnits();
+    void addAllowedRole(Role role);
+    void removeAllowedRole(Role role);
+    List<Role> getAllowedRoles();
+    boolean hasEmployee(Employee employee);
+    void printStructure();
+    boolean hasSubUnits();
+    boolean hasEmployees();
+
+
+}
