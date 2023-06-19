84a6323e51da2c39ef45b98596b753fdf3106486
Simon <100418694+SimonXX@users.noreply.github.com>
2023-06-19 23:22:49 +0200

    Implementazione classe OrganigrammaModel

--- src/ObserverGUI/OrganigrammaModel.java
+++ src/ObserverGUI/OrganigrammaModel.java
@@ -1,4 +1,30 @@
 package ObserverGUI;
 
+
+import Composite.Azienda;
+import Composite.Unit;
+
 public class OrganigrammaModel {
+    //la classe OrganigrammaModel rappresenta il modello dell'organigramma e si occupa della gestione dei dati e della logica dell'organigramma.
+
+    private Unit organigramma;
+    private Azienda azienda;
+
+    public OrganigrammaModel(Unit rootUnit, Azienda azienda) {
+        this.organigramma = rootUnit;
+        this.azienda = azienda;
+
+    }
+    public Unit getOrganigramma(){
+        return organigramma;
+    }
+
+    public void setOrganigramma(Unit organigramma){
+        this.organigramma = organigramma;
+    }
+
+    public Azienda getAzienda(){
+        return azienda;
+    }
+
 }
